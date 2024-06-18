0cac80b37f38d7b9e7c6900b8184a1e44c865674
J.Jaya surya moison <jayasuruas274@gmail.com>
2024-06-18 19:26:48 +0530

    chatgpt

---
+++ app.js
@@ -0,0 +1,6 @@
+let sideBar = document.getElementById('sideBar')
+let mainScreen = document.getElementById('mainScreen')
+closeSideBar=()=>{
+// sideBar.classList.add('hidden')
+// mainScreen.style.width = '100%'
+}
